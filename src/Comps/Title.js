3c34f75ce5289a8cd55c6dc116613c5751ce4a3c
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2021-09-27 00:02:24 -0700

    Update Title.js
    save

--- src/Comps/Title.js
+++ src/Comps/Title.js
@@ -1,20 +1,13 @@
 import React from 'react';
-
-
-
 const Title = () => 
 {
   return (
     <>
       <div className="title">
         <h1>StuffYourPics</h1>
-
-
-        <h2 className="mybase ny-3"  >Remembering old memories gives you big smile on your face</h2>
+         <h2 className="mybase ny-3"  >Remembering old memories gives you big smile on your face</h2>
         <p>The best moments are the unplanned ones that just happen but make your day perfect !</p>
-
-
-      </div>
+  </div>
       </>
       );
       
