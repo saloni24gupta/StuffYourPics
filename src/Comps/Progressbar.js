f0918feb0824901e1d7626b2dd9e2e933a456e0c
Saloni Gupta <70319586+saloni24gupta@users.noreply.github.com>
2021-09-26 23:57:45 -0700

    Update Progressbar.js
    save

--- src/Comps/Progressbar.js
+++ src/Comps/Progressbar.js
@@ -4,18 +4,16 @@ import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 const ProgressBar = ({file, setFile}) => {
    const { url, progress } = useStorage(file);
-useEffect(()=> {
+useEffect(() => {
      if(url)
      {
          setFile(null);
      }
-
 }, [url, setFile]);
 
  
    return (
-    
         <motion.div className='progress-bar' initial={{width: 0}} animate={{ width: progress + '%' }}>progress bar</motion.div>
-    )
+         );
 }
 export default ProgressBar;
